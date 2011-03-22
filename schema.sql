e2f59c043f29de28e4f4943ca9d9503a2ca57c07
Cal Henderson <cal@iamcal.com>
2011-03-21 21:58:55 -0500

    the schema for the single table

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `when` int(10) unsigned NOT NULL,
+  `chan` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `event` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `chan` (`chan`,`when`),
+  FULLTEXT KEY `text` (`text`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
